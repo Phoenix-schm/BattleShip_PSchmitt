f05d1960594e8ac162652d10f7799bc761a5b746
Phoenix-schm <ellaschmitt2016@gmail.com>
2025-02-28 23:02:01 -0600

    Created title screen Ascii art as string[] Display title screen through foreach loop. Created MainMenuChoices enum. Can display each MainMenuChoices enum in titlescreen.

--- BattleShip_PSchmitt/Program.cs
+++ BattleShip_PSchmitt/Program.cs
@@ -2,9 +2,50 @@
 {
     internal class Program
     {
+        public enum MainMenuChoices
+        {
+            Invalid,
+            Plaver_Vs_CPU,
+            Player_Vs_Player,
+            Quit
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] titleScreen =
+            {
+                " ____        _   _   _           _     _       ",
+                "| __ )  __ _| |_| |_| | ___  ___| |__ (_)_ __  ",
+                "|  _ \\ / _` | __| __| |/ _ \\/ __| '_ \\| | '_ \\ ",
+                "| |_) | (_| | |_| |_| |  __/\\__ \\ | | | | |_) |",
+                "|____/ \\__,_|\\__|\\__|_|\\___||___/_| |_|_| .__/ ",
+                "                                        |_|    \r\n",
+            };
+
+            foreach (string row in titleScreen)
+            {
+                Console.WriteLine(row);
+            }
+            Console.WriteLine("Press any key to start...");
+            Console.ReadKey();
+            Console.WriteLine();
+            //start game here
+            Console.WriteLine("Choose your game:");
+            DisplayMainMenuChoices();
+        }
+        static void DisplayMainMenuChoices()
+        {
+            foreach(MainMenuChoices choice in Enum.GetValues(typeof(MainMenuChoices)))
+            {
+                string choiceString = choice.ToString().Replace("_", " ");
+                if (choiceString == "Invalid")
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine((int)choice + ") " + choiceString);
+                }
+            }
         }
     }
 }
