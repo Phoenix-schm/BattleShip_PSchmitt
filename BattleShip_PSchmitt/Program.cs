fa058db31eb3f175b73d15eb436297ba1c9710d1
Phoenix-schm <ellaschmitt2016@gmail.com>
2025-02-28 22:34:20 -0600

    Add project files.

---
+++ BattleShip_PSchmitt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleShip_PSchmitt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
